61b03e8277b4ae94ee3f8966a8f594e22dd43458
.\.git\CharmingVanilla <hongyanli01@outlook.com>
2025-06-16 10:40:04 +0200

    revise enviroment parameters

--- app/database.py
+++ app/database.py
@@ -5,9 +5,12 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from .config import setting
+import os
 
-SQLALCHEMY_DATABASE_URL=f'postgresql://{setting.database_username}:{setting.database_password}@{setting.database_hostname}:{setting.database_port}/{setting.database_name}'
-engine=create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+#SQLALCHEMY_DATABASE_URL=f'postgresql://{setting.database_username}:{setting.database_password}@{setting.database_hostname}:{setting.database_port}/{setting.database_name}'
+engine=create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 Base=declarative_base()
